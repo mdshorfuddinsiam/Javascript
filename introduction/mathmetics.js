0ad569f2d3a67d949d652ea000b3f5c038c6ec65
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-03-28 19:59:53 +0600

    Mathmetics matter with string type data

---
+++ introduction/mathmetics.js
@@ -0,0 +1,31 @@
+// var first = 15;
+var first = 40;
+// var second = 30;
+var second = '30';
+var total;
+
+// var total = first + second;
+// console.log(total);
+
+// total += 10;
+// console.log(total);
+
+// total -= 10;
+// console.log(total);
+
+second -= 10;
+console.log(typeof second);
+console.log(second);
+
+// total *= 10;
+// console.log(total);
+
+// total *= 10;
+// console.log(total);
+
+// total /= 10;
+// console.log(total);
+
+// total %= 10;
+// console.log(total);
+

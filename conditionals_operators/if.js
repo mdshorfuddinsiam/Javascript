5ff2d165d542569e2428926c6bb5e33eef7b04e0
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-03-29 02:13:26 +0600

    Simple if condition example

---
+++ conditionals_operators/if.js
@@ -0,0 +1,15 @@
+if(5 < 10){
+    console.log('This condition is TRUE');
+}
+
+if(5 > 10){
+    console.log('This condition is False');
+}
+
+// var weight = 50;
+var weight = 49;
+if(weight < 50){
+    console.log('I will carry her');
+}else{
+    console.log('She is too heavy');
+}

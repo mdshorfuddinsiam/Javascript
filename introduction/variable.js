a52bb19e63f50044eadba3330b8a8611e92a563c
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-03-28 16:33:04 +0600

    Undefined variable

--- introduction/variable.js
+++ introduction/variable.js
@@ -1,9 +1,15 @@
 var name = 'Siam';
 var age = 27;
 var weight = 58;
+var year;
 
 weight = 55;
 
 console.log(name);
 console.log(age);
 console.log(weight);
+console.log(year);
+
+const car = 'Toyota';
+car = 'Volvo';
+console.log(car);

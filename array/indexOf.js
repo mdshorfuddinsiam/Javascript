33eadb23eac6c987cb56fdedea62af74cf4fc727
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-03-30 01:42:48 +0600

    indexOf example

---
+++ array/indexOf.js
@@ -0,0 +1,8 @@
+const ages = [];
+const numbers = [40, 34, 89, 300, 12, 9, 75];
+
+console.log(numbers);
+console.log(numbers.indexOf('12'));
+console.log(numbers.indexOf(12));
+console.log(numbers.indexOf(33));
+

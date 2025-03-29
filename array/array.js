fda0ac678648080dee63315efa1d353954305afc
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-03-30 00:14:52 +0600

    Array basic example

---
+++ array/array.js
@@ -0,0 +1,21 @@
+const numbers = [12, 55, 98, 42, 75];
+const cars = ['toyota', 'volvo', 'tesla', 'ferrari', 'lamborgini'];
+const passed = [true, false, true, false, true, false, true, false];
+const mixedArray = [12, 'Magura', true, 34.369];
+
+// console.log(numbers);
+// console.log(cars);
+// console.log(passed);
+// console.log(mixedArray);
+
+// Array Length
+// console.log('Array (numbers) : ' + numbers.length);
+// console.log('Array (cars) : ' + cars.length);
+// console.log('Array (passed) : ' + passed.length);
+// console.log('Array (mixedArray) : ' + mixedArray.length);
+
+// console.log(cars[3]);
+var car_name = cars[3];
+console.log(car_name);
+cars[3] = "mazda";
+console.log(cars);

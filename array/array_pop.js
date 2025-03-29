bdcb7162afd6e9a530afffba6b05ab81bb9a10e3
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-03-30 00:27:36 +0600

    Array pop example

---
+++ array/array_pop.js
@@ -0,0 +1,10 @@
+const ages = [];
+const numbers = [40, 34, 89, 300, 12, 9, 75];
+
+console.log(numbers);
+const out1 = numbers.pop();
+console.log(numbers);
+const out2 = numbers.pop();
+// numbers.pop(300);    // not working
+console.log(numbers);
+console.log(out1, out2);

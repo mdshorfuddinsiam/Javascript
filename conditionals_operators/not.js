bf8cecc56135eb18971c2f51ae338f87fa12ab0b
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-03-29 20:34:41 +0600

    Not condition example

---
+++ conditionals_operators/not.js
@@ -0,0 +1,10 @@
+var isPassed = false;
+
+// if(isPassed === false){
+// if(isPassed){   // check : isPassed === true
+// if(!isPassed){   // check : isPassed !== true
+if(!!isPassed){   
+    console.log('Not Certified');
+}else{
+    console.log('Your are Certified');
+}

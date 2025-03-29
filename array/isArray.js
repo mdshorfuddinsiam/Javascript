861d713b1143fecfdcb25b0611f226d7524edbbb
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-03-30 01:51:22 +0600

    isArray example

---
+++ array/isArray.js
@@ -0,0 +1,12 @@
+const cars = [];
+const numbers = [40, 34, 89, 300, 12, 9, 75];
+const fullName = "Md. shorfuddin (siam)";
+const age = 27;
+
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(cars));
+console.log(Array.isArray(fullName));
+console.log(Array.isArray(age));
+
+
+

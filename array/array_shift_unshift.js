95cc85fcc70bf321f8e56875c082bcc575fca419
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-03-30 01:34:24 +0600

    Array shift unshift example

---
+++ array/array_shift_unshift.js
@@ -0,0 +1,17 @@
+const ages = [];
+const names = ['salam', 'kalam', 'kuddus', 'shafiq'];
+
+console.log(names);
+const out1 = names.shift();
+console.log(names);
+const out2 = names.shift();
+console.log(names);
+console.log(out1, out2);
+
+
+// console.log(names);
+// const out1 = names.unshift('kumar');
+// console.log(names);
+// const out2 = names.unshift('dalim');
+// console.log(names);
+// console.log(out1, out2);
---
+++ array/includes.js
@@ -0,0 +1,7 @@
+const ages = [];
+const numbers = [40, 34, 89, 300, 12, 9, 75];
+
+console.log(numbers);
+console.log(numbers.includes('12'));
+console.log(numbers.includes(12));
+console.log(numbers.includes(33));

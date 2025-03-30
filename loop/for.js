d1cc9ff2eee776e12c86c445737587760cb4172f
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-03-30 20:59:43 +0600

    More for loop example

--- loop/for.js
+++ loop/for.js
@@ -34,3 +34,56 @@ console.log('Result (sum) : ' + sum);
 for(let i=20; i>=11; i--){
     console.log('Decrement : '+i);
 }
+
+// Even
+for(let i=1; i<=20; i++){
+    if(i % 2 === 0){
+        console.log('Even number : ' + i);
+    }
+}
+
+// Even
+for(let i=1; i<=20; i++){
+    // if(i % 2 === 1){
+    if(i % 2 !== 0){
+        console.log('Odd number : ' + i);
+    }
+}
+
+// give me the list numbers between 1 to 30 divisible by 5
+for(let i=1; i<=30; i++){
+    if(i % 5 === 0){
+        console.log('Divisible (5) : ' + i);
+    }
+}
+
+// give me the list numbers between 1 to 30 divisible by 3
+for(let i=1; i<=30; i++){
+    if(i % 3 === 0){
+        console.log('Divisible (3) : ' + i);
+    }
+}
+
+// give me the list numbers between 1 to 30 divisible by 3 & 5
+for(let i=1; i<=30; i++){
+    if(i%3 === 0 && i%5 === 0){
+        console.log('Divisible (3 & 5) : ' + i);
+    }
+}
+
+// give me the list numbers between 1 to 30 divisible by 3 || 5
+for(let i=1; i<=30; i++){
+    if(i%3 === 0 || i%5 === 0){
+        console.log('Divisible (3 or 5) : ' + i);
+    }
+}
+
+// give me the sum of from numbers 1 to 20 that are divisible by 3 
+let total = 0;
+for(let i=1; i<=20; i++){
+    if(i % 3 === 0){
+        total += i;
+        console.log('Total : ' + total);
+    }
+}
+console.log('Total Sum : ' + total);

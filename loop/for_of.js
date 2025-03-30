dd99a759eb3b42abaa7aa8afa9f24e0b4fc15455
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-03-30 16:12:40 +0600

    For of loop example

---
+++ loop/for_of.js
@@ -0,0 +1,11 @@
+var numbers = [10, 20, 30, 40, 50, 60];
+console.log(numbers);
+for(var num of numbers){
+    console.log(num);
+}
+
+var fruits = ['Apple', 'Grape', 'Guava', 'Orange', 'Mango'];
+console.log(numbers);
+for(var fruit of fruits){
+    console.log(fruit);
+}

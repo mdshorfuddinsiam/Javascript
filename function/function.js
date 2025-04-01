e53bde48c29ece3dd0de6cb092aa74352611d374
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-04-02 01:24:15 +0600

    Simple Function example

---
+++ function/function.js
@@ -0,0 +1,11 @@
+function profile(){
+    console.log('Name: Md. Shorfuddin (siam)');
+    console.log('Age: 26');
+    console.log('Sex: Male');
+}
+
+profile();
+console.log('--------------------');
+console.log('--------------------');
+console.log('--------------------');
+profile();

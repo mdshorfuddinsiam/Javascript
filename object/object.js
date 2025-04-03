089e6f0d1598018ad56a534b0a396c404e41485b
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-04-03 20:07:21 +0600

    object example

---
+++ object/object.js
@@ -0,0 +1,38 @@
+// primitive
+// basic, primary
+
+const age = 26;
+const school = 'model school';
+const isPassed = true;
+// const isDeveloper;
+
+// console.log(isDeveloper);
+
+const subjects = ['bangla', 'english', 'physics', 'math'];
+const bottles = ['white', 45];
+const bottleColor = 'while';
+const bottlePrice = 45;
+const bottleDrinks = 'water';
+
+// object
+// non-primitive
+const waterBottle = {
+    brand : 'mum',
+    weight : '500ml',
+    price : 20,
+    isBSTI : true
+}
+
+const subject = {
+    name : 'Bangla',
+    teacher : 'aaa',
+    examDate : '30 April',
+    chapters : ['first', 'second', 'third'],
+    exams : {
+        name : 'Final Exam',
+        marks : 100,
+    }
+}
+
+console.log(waterBottle);
+console.log(subject);

c6d86e3dd62cd730d5f8364e216a2e0c9d4fc739
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-04-03 20:16:40 +0600

    Property access example

---
+++ object/object_properties.js
@@ -0,0 +1,17 @@
+let person = {
+    name: 'siam',
+    age: 27,
+    profession: 'developer',
+    salary: 15000,
+    'pro languages': ['html', 'css', 'javascript'],
+}
+
+// dot notation ---> dot diye property access kora hoy
+console.log(person);
+console.log(person.profession);
+let income = person.salary;
+console.log(income);
+
+// bracket notation ---> third bracket diye property access kora hoy
+console.log(person['name']);
+console.log(person['age']);

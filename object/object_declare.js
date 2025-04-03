4cb80468a7956f88968a3315d650d376b150ef0b
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-04-03 21:45:33 +0600

    Object declare example

---
+++ object/object_declare.js
@@ -0,0 +1,6 @@
+const pen = { brand: 'econo', price: 10, color: 'black'}
+const pencil = new Object();
+console.log(pencil);
+const rubber = Object.create({});   // kivabe kaj kore?
+// const rubber = Object.create({ brand: 'xxx', price: 5, color: 'white'});    // why kaj kore nah
+console.log(rubber);
